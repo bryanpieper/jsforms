24e2dfd4fb73e0b901017b226ef0e8f40b6c6eab
Bryan Pieper <bryanpieper@gmail.com>
2011-02-12 08:58:06 -0500

    Added requestHandler, errorHandler

--- static/js/jquery-jsforms.js
+++ static/js/jquery-jsforms.js
@@ -34,14 +34,18 @@
 			url: undefined,
 			cache: false,
 			loaderElement: undefined,
-			success:function(data) {},
-			error:function(obj) {},
-			data:function(form) {
+			requestSuccess: undefined,
+			requestError: undefined,
+			data: function(form) {
 				// Serializes the form as-is and ensures all fields
 				// are included in submit
 				return form.serialize();
 			},
-			preSubmit:function(form) {}
+			preSubmit: function(form) {},
+			// provide an error handler function if you wish to customize
+			// the error behavior
+			errorHandler: undefined,
+			successHandler: undefined
 		};
 	
 		return this.each(function() {
@@ -83,51 +87,67 @@
 				// Handles the successful response from the webserver
 				var successCallback = function(data) {
 					
-					// Removes any leftover error elements
-					$form.children(".error").removeClass("error").find("ul").remove();
-					$form.children("ul.errorlist").remove();
+					if (typeof settings.errorHandler === "undefined") {
+						// Removes any leftover error elements
+						$form.children(".error").removeClass("error").find("ul").remove();
+						$form.children("ul.errorlist").remove();
+					}
 
 					if (typeof data.errors !== "undefined" && data.errors) {
 						
-						// Creates the error elements and adds appropriate .error 
-						// classes to the form
-						// 
-						//  <div class=".field_name .field">
-						//    <ul>
-						//      <li>error msg 1</li>
-						//      <li>error msg 2</li>
-						//    </ul>
-						//    <input name="field_name" type="text" id="id_field_name" />
-						//  </div>
-						// 
-						// For the __all__ error case, will display the <ul> at end top of 
-						// the form. Will have the 'errorlist' class
-						for (var e in data.errors) {
-							// The errors dict contains the names of the fields. Each contains
-							// a list of the error messages. 
-							var errors = data.errors;
-							var ul = document.createElement("ul");
-							for (var i=0; i<errors[e].length; i++) {
-								var li = document.createElement("li");
-								li.innerHTML = errors[e][i];
-								ul.appendChild(li);
+						if (typeof settings.errorHandler === "undefined") {
+							// Creates the error elements and adds appropriate .error 
+							// classes to the form
+							// 
+							//  <div class=".field_name .field">
+							//    <ul>
+							//      <li>error msg 1</li>
+							//      <li>error msg 2</li>
+							//    </ul>
+							//    <input name="field_name" type="text" id="id_field_name" />
+							//  </div>
+							// 
+							// For the __all__ error case, will display the <ul> at end top of 
+							// the form. Will have the 'errorlist' class
+							for (var e in data.errors) {
+								// The errors dict contains the names of the fields. Each contains
+								// a list of the error messages. 
+								var errors = data.errors;
+								var ul = document.createElement("ul");
+								for (var i=0; i<errors[e].length; i++) {
+									var li = document.createElement("li");
+									li.innerHTML = errors[e][i];
+									ul.appendChild(li);
+								}
+								if (e == "__all__") {
+									ul.className = "errorlist";
+									$form.children().first().before(ul);
+								} else {
+									$form.children("." + e).addClass("error").children("label").before(ul);
+								}
 							}
-							if (e == "__all__") {
-								ul.className = "errorlist";
-								$form.children().first().before(ul);
-							} else {
-								$form.children("." + e).addClass("error").children("label").before(ul);
-							}
+						} else {
+							// call the errorHandler for each field with errors
+							for (var e in data.errors) {
+								settings.errorHandler(e, data.errors[e]);
+							}							
 						}
 						
 					} else {
-						if (typeof data.redirect && data.redirect) {
-							window.location.href = data.redirect;
+						if (typeof settings.successHandler === "undefined") {
+							// Call the redirect if available
+							if (typeof data.redirect && data.redirect) {
+								window.location.href = data.redirect;
+							}
+						} else {
+							// Call the successHanlder if no errors
+							settings.successHandler(data);
 						}
 					}
 					
-					if (typeof settings.success !== "undefined") {
-						settings.success(data);
+					// Call the requestSuccess if Ajax call succeeded
+					if (typeof settings.requestSuccess !== "undefined") {
+						settings.requestSuccess(data);
 					}
 					
 					loader.hide();
@@ -136,8 +156,8 @@
 				// Handles the unsuccessful response from the webserver
 				var errorCallback = function(obj) {
 					loader.hide();
-					if (typeof settings.error !== "undefined") {
-						settings.error(obj);
+					if (typeof settings.requestError !== "undefined") {
+						settings.requestError(obj);
 					}
 				};
 				
